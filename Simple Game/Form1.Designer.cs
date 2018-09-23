2c76a03505db5dbd38ae32fc1d45df51428311fc
srujanpothina <srujan4948@gmail.com>
2018-09-23 15:42:08 -0400

    Full game is on. Come play it.

--- Simple Game/Form1.Designer.cs
+++ Simple Game/Form1.Designer.cs
@@ -29,12 +29,122 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Slider = new System.Windows.Forms.PictureBox();
+            this.Vert = new System.Windows.Forms.Timer(this.components);
+            this.Horz = new System.Windows.Forms.Timer(this.components);
+            this.ExitApp = new System.Windows.Forms.PictureBox();
+            this.key_watch = new System.Windows.Forms.Timer(this.components);
+            this.pblpause = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitApp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.Enabled = false;
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(7, 91);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(66, 61);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            this.Ball.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Slider
+            // 
+            this.Slider.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Slider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Slider.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Slider.Location = new System.Drawing.Point(46, 439);
+            this.Slider.Name = "Slider";
+            this.Slider.Size = new System.Drawing.Size(153, 13);
+            this.Slider.TabIndex = 1;
+            this.Slider.TabStop = false;
+            this.Slider.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Vert
+            // 
+            this.Vert.Enabled = true;
+            this.Vert.Interval = 1;
+            this.Vert.Tick += new System.EventHandler(this.Vert_Tick);
+            // 
+            // Horz
+            // 
+            this.Horz.Enabled = true;
+            this.Horz.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ExitApp
+            // 
+            this.ExitApp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitApp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitApp.Image = ((System.Drawing.Image)(resources.GetObject("ExitApp.Image")));
+            this.ExitApp.Location = new System.Drawing.Point(899, 3);
+            this.ExitApp.Name = "ExitApp";
+            this.ExitApp.Size = new System.Drawing.Size(19, 22);
+            this.ExitApp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ExitApp.TabIndex = 3;
+            this.ExitApp.TabStop = false;
+            this.ExitApp.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // key_watch
+            // 
+            this.key_watch.Enabled = true;
+            this.key_watch.Tick += new System.EventHandler(this.key_watch_Tick);
+            // 
+            // pblpause
+            // 
+            this.pblpause.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pblpause.BackColor = System.Drawing.Color.Black;
+            this.pblpause.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pblpause.ForeColor = System.Drawing.Color.White;
+            this.pblpause.Location = new System.Drawing.Point(1, 3);
+            this.pblpause.Name = "pblpause";
+            this.pblpause.Size = new System.Drawing.Size(917, 64);
+            this.pblpause.TabIndex = 1;
+            this.pblpause.Text = "Game Paused";
+            this.pblpause.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.pblpause.Visible = false;
+            this.pblpause.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(42)))), ((int)(((byte)(53)))));
+            this.ClientSize = new System.Drawing.Size(917, 454);
+            this.Controls.Add(this.ExitApp);
+            this.Controls.Add(this.pblpause);
+            this.Controls.Add(this.Slider);
+            this.Controls.Add(this.Ball);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitApp)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Slider;
+        private System.Windows.Forms.Timer Vert;
+        private System.Windows.Forms.Timer Horz;
+        private System.Windows.Forms.PictureBox ExitApp;
+        private System.Windows.Forms.Timer key_watch;
+        private System.Windows.Forms.Label pblpause;
     }
 }
 
--- Simple Game/Form1.cs
+++ Simple Game/Form1.cs
@@ -16,5 +16,139 @@ namespace Simple_Game
         {
             InitializeComponent();
         }
+        int speed = 5;
+        int slide_speed = 10;
+
+        bool game_over = false;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int hdir = +1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Ball.Left < 0)
+            {
+                hdir = +1;
+            }
+            else if (Ball.Left > (this.Width - Ball.Width))
+            {
+                hdir = -1;
+            }
+            
+             Ball.Left += (hdir* speed);
+            
+        }
+
+        int vdir = +1;
+        private void Vert_Tick(object sender, EventArgs e)
+        {
+            if(Ball.Top < 0)
+            {
+                vdir = +1;
+            }
+            else if (Ball.Top > (this.Height - Ball.Height-Slider.Height))
+            {
+
+                if(Ball.Left < Slider.Left || ((Ball.Left + Ball.Width ) > (Slider.Left + Slider.Width)))
+                {
+                    Vert.Enabled = Horz.Enabled = false;
+                    game_over = true;
+                    pblpause.Visible = true;
+                    pblpause.BackColor = Color.DarkRed;
+                    pblpause.Text = "Game Over !";
+                    speed = 5;
+                    //MessageBox.Show('Game Over'); 
+                }
+                else
+                {
+                    speed++;
+                }
+                vdir = -1;
+            }
+           
+            Ball.Top += (vdir* speed);
+            
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        bool _left = false, _right = false;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // active
+            if (e.KeyCode == Keys.Right)
+            {
+                _right = true;
+            }
+            else if(e.KeyCode == Keys.Left)
+            {
+                _left = true;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                // pause game 
+                pblpause.BackColor = Color.Black;
+                pblpause.Text = "Game Paused";
+                Vert.Enabled = Horz.Enabled = paused = false;
+
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                // resume game
+                Vert.Enabled = Horz.Enabled = paused = true;
+            }
+        }
+        bool paused
+        {
+            get {
+                return !pblpause.Visible;
+            }
+            set {
+                pblpause.Visible = !value;
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // inactive
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                _right = _left = false;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void key_watch_Tick(object sender, EventArgs e)
+        {
+            if (_right && Slider.Left<(this.Width - Slider.Width))
+            {
+                Slider.Left = Slider.Left + 20;
+            }
+            else if(_left && Slider.Left > 0)
+            {
+                Slider.Left = Slider.Left - 20;
+            }
+        }
+
+       
     }
 }
